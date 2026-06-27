1929d2c1bafae64ede72b4d1fc60e9bb70f0491c
anonymise6022 <laukelvin123123@gmail.com>
2026-06-27 10:56:26 -0400

    Frontend debug

---
+++ frontend/ios/recording_bridge.c
@@ -0,0 +1,15 @@
+//
+//  recording_bridge.c
+//  
+//
+//  Created by Kelvin Lau on 2026-06-27.
+//
+#include <stdio.h>
+
+void start_recording(void) {
+    // your real recording logic goes here later
+}
+
+void stop_recording(void) {
+    // your real stop logic goes here later
+}
