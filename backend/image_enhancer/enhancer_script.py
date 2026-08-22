11b1ab30ad883a33703bead2d134c13936a3cf4c
Kingsley Leon <kingsleyleoncs@gmail.com>
2026-08-22 14:25:06 -0400

    bug fix: All notes are not filled when its filled and enhancer successfully runs, however the performance is not as optimized and next version will use 1 time parsing and a 2 pass 3x3 matrix

--- backend/image_enhancer/enhancer_script.py
+++ backend/image_enhancer/enhancer_script.py
@@ -6,29 +6,39 @@ from pyvips import Image
 import multiprocessing
 import traceback
 import img2pdf
+import time
+
+# CONSTANTS
+MAX_WORKERS: int = 4
+MAX_PAGES_BASE: int = 25
 
 
 # Private Function to this file
 def enhance_page(input_pdf_path: str, page_num: int) -> Image:
     """Process a single page of the pdf by executing different operations to intensify wanted pixels and minimize unwanted ones"""
     img: Image = cast(Image, Image.pdfload(input_pdf_path, dpi=400, page=page_num, access="sequential"))
-    gray: Image = img.colourspace("b-w")  # type: ignore
+    gray: Image = img.colourspace("b-w").cast("uchar")  # type: ignore
     
-    blur = gray.gaussblur(2) # type: ignore
-    binary_clear = (gray < (blur - 15)).cast("uchar") * 255 # type: ignore
+    blur = gray.gaussblur(15) # type: ignore
+    binary_clear = (gray >= (blur - 15)).cast("uchar") * 255 # type: ignore
 
     # FIX 1: Wrap the Python list inside an explicit pyvips Image array
-    mask_structure = [[255, 255, 255], [255, 255, 255], [255, 255, 255]]
+    mask_structure = [
+        [255, 255, 255], 
+        [255, 255, 255], 
+        [255, 255, 255]
+    ]
     mask_matrix = Image.new_from_array(mask_structure)
     
-    return binary_clear.morph(mask_matrix, "erode") # type: ignore
+    dilated = binary_clear.morph(mask_matrix, "dilate")
+    return dilated.morph(mask_matrix, "erode")
 
 # Private Function to this file
 def enhance_music_pdf(input_pdf_path: str, output_pdf_path: str):
     """Process an entire music pdf"""
     try:
-        pdf = Image.pdfload(input_pdf_path, page=0, n=1)
-        n_pages: int = pdf.get("n-pages") # type: ignore
+        meta = Image.pdfload(input_pdf_path, page=0, n=1)
+        n_pages: int = meta.get("n-pages") # type: ignore
         base_name = os.path.splitext(os.path.basename(input_pdf_path))[0]
         # Get the directory where you want to save the output
         output_dir = os.path.dirname(output_pdf_path)
@@ -39,7 +49,7 @@ def enhance_music_pdf(input_pdf_path: str, output_pdf_path: str):
         for p in range(n_pages):
             page = enhance_page(input_pdf_path=input_pdf_path, page_num=p)
             page_output_path = os.path.join(output_dir, f"{base_name}_page_{p+1}.png")
-            page.write_to_file(page_output_path, page_height=pdf[0].height) 
+            page.write_to_file(page_output_path, page_height=meta[0].height) 
         return True
         
     except Exception as e:
